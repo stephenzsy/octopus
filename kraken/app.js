d2607436f0e8df02f6b00f4bbe4bc2c90a29072b
Stephen <stephen.zsy@gmail.com>
2015-01-17 00:02:51 -0800

    added auth handler

--- kraken/app.js
+++ kraken/app.js
@@ -1,16 +1,16 @@
 'use strict';
 
-var thrift = require('thrift');
+var serverFactory = require('./lib/kraken/server/auth_enabled_web_json_server');
 
 var KrakenService = require('kraken-model').Service;
 
-module.exports = thrift.createWebServer({
+module.exports = serverFactory.createWebServer({
     cors: {'*': true},
     services: {
         '/': {
             processor: KrakenService,
             handler: require('./lib/kraken/handler'),
-            protocol: thrift.TJSONProtocol
+            requireAuth: false
         }
     }
 });
