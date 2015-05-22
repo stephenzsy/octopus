f1cdaef6e52a871cc49ea47d2bceacdda831b9c3
Stephen Zhang <stephen.zsy@gmail.com>
2015-05-22 11:25:12 -0400

    added missing file

---
+++ dolphin/app/document/article.ts
@@ -0,0 +1,43 @@
+﻿import ArticleSource = require('../models/article-source');
+
+class Article {
+    private articleSource: ArticleSource;
+    private _metadata: { [name: string]: string } = {};
+
+    constructor(articleSource: ArticleSource) {
+        this.articleSource = articleSource;
+    }
+
+    // data representation
+    get articleSourceId(): string {
+        return this.articleSource.Id;
+    }
+    archiveBucket: string;
+    articleId: string;
+    get archiveBucketId(): string {
+        return this.articleSourceId + ':' + this.archiveBucket;
+    }
+
+    get metadata(): { [name: string]: string } {
+        return this._metadata;
+    }
+
+    set metadata(value: { [name: string]: string }) {
+        for (var name in value) {
+            this._metadata[name] = value[name];
+        }
+    }
+
+    get sourceUrl(): string {
+        return this.metadata['source-url'];
+    }
+    set sourceUrl(value: string) {
+        this.metadata['source-url'] = value;
+    }
+
+    properties: {
+        title?: string;
+    } = {};
+}
+
+export = Article;
